07d2daa4d126fa0522a9a4df7bcb81cdaf17fbd5
parnuk3 <parnuk3@gmail.com>
2018-07-26 13:31:52 +0500

    Добавил переключения слайдера

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-var currentSlide = Document.querySelector(".slider-btn:checked");
-var classList = Document.querySelector(".container").classList;
-classList.add("first-bg");
+var bg = document.querySelector(".contain");
+var backgrounds = ["bg1", "bg2", "bg3"]
+
+
+function slider () {
+    
+    var slide = document.querySelector(".slider-btn:checked").dataset.bgs;
+    for (var i = 0; i < backgrounds.length; i++) {
+        bg.classList.remove(backgrounds[i]);
+    }
+    bg.classList.add(slide);
+}
+
+
+
