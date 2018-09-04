8ed98db9b2c60b4dfedbe52701ad66d4265925c2
parnuk3 <parnuk3@gmail.com>
2018-09-04 20:02:15 +0500

    Начал писать скрипт на добавление элементов

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var bg = document.querySelector(".contain");
-var backgrounds = ["bg1", "bg2", "bg3"]
+var backgrounds = ["bg1", "bg2", "bg3"];
 
 
-function slider () {
+var slider = function () {
     
     var slide = document.querySelector(".slider-btn:checked").dataset.bgs;
     for (var i = 0; i < backgrounds.length; i++) {
@@ -11,5 +11,41 @@ function slider () {
     bg.classList.add(slide);
 }
 
+var product1 = {
+    price: '310Р/кг',
+    description: 'Сливочное с апельсиновым джемом и цитрусовой стружкой',
+    imgUrl: 'img/raspberries.jpg'
+}
+
+var makeElement = function (tagName, className, textContent) {
+    var element = document.createElement(tagName);
+    element.classList.add(className);
+    if (textContent) {
+        element.textContent = textContent;
+    }
+    return element;
+}
+
+var createCard = function (product) {
+    var listItem = makeElement ('li', 'hot-offers-item');
+    var pictures = makeElement ('img', 'hot-offers-image');
+    pictures.src = product.imgUrl;
+    pictures.alt =  product.description;
+    listItem.appendChild(pictures);
+    var hit = makeElement ('img', 'hot-offers-hit');
+    hit.src = '#';
+    hit.alt =  'Хит';
+    listItem.appendChild(hit);
+    var description = makeElement ('p', 'hot-offers-description', product.description);
+    listItem.appendChild(description);
+    var price = makeElement ('span', 'hot-offers-price', product.price);
+    listItem.appendChild(price);
+    
+    return listItem;
+}
+
+var cardList = document.querySelector('.hot-offers');
+var cardItem = createCard (product1);
+cardList.appendChild(cardItem);
 
 
