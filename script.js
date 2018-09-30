23b2f79fceb43797559d32a15c0c0a3296198f79
parnuk3 <parnuk3@gmail.com>
2018-09-30 20:27:00 +0500

    добавил два попап окна

--- script.js
+++ script.js
@@ -11,54 +11,33 @@ var slider = function () {
     bg.classList.add(slide);
 }
 
-var dataCardHit = [
-	{
-		price: '310₽',
-    	description: 'Сливочное с апельсиновым джемом и цитрусовой стружкой',
-    	imgUrl: 'img/card1.jpg'	
-	},
-	{
-		price: '380₽',
-    	description: 'Сливочно-кофейное с кусочками шоколада',
-    	imgUrl: 'img/card2.jpg'
-	},
-	{
-		price: '355₽',
-    	description: 'Сливочно-клубничное с присыпкой из белого шоколада',
-    	imgUrl: 'img/card3.jpg'
-	},
-	{
-		price: '415₽',
-    	description: 'Сливочное крем-брюле с карамельной подливкой',
-    	imgUrl: 'img/card4.jpg'
-	}
-    
-];
-
 var dataCard = [
 	{
 		price: '310₽',
     	description: 'Сливочное с апельсиновым джемом и цитрусовой стружкой',
-    	imgUrl: 'img/card1.jpg'	
+    	imgUrl: 'img/card1.jpg',
+		isHit: true
 	},
 	{
 		price: '380₽',
     	description: 'Сливочно-кофейное с кусочками шоколада',
-    	imgUrl: 'img/card2.jpg'
+    	imgUrl: 'img/card2.jpg',
+		isHit: true
 	},
 	{
 		price: '355₽',
     	description: 'Сливочно-клубничное с присыпкой из белого шоколада',
-    	imgUrl: 'img/card3.jpg'
+    	imgUrl: 'img/card3.jpg',
+		isHit: true
 	},
 	{
 		price: '415₽',
     	description: 'Сливочное крем-брюле с карамельной подливкой',
-    	imgUrl: 'img/card4.jpg'
+    	imgUrl: 'img/card4.jpg',
+		isHit: true
 	}
     
 ];
-
 
 
 
@@ -77,11 +56,9 @@ var createCard = function (product, isHit) {
     pictures.src = product.imgUrl;
     pictures.alt =  product.description;
     listItem.appendChild(pictures);
-    if (isHit) {
-        var hit = makeElement ('img', 'product-hit');
-        hit.src = 'img/hit.png';
-        hit.alt =  'Хит';
-        listItem.appendChild(hit);
+	listItem.classList.remove("product-hit");
+    if (product.isHit) {
+        listItem.classList.add("product-hit");
     }
     var description = makeElement ('p', 'product-description', product.description);
     listItem.appendChild(description);
@@ -93,17 +70,94 @@ var createCard = function (product, isHit) {
     return listItem;
 }
 
-var renderProducts = function (productsArray, parentObject, isHit) {
+var renderProducts = function (productsArray, parentObject) {
     
     var cardList = document.querySelector(parentObject);
     for (var i = 0; i < productsArray.length; i++) {
         var currentCard = productsArray[i];
-        var cardItem = createCard (currentCard, isHit);
+        var cardItem = createCard (currentCard);
         cardList.appendChild(cardItem);
     }
 }
 
+var hitList = document.querySelector('.hot-offers');
+if (hitList) {
+	var dataCardHit = [];
+	for (var i = 0; i < dataCard.length; i++) {
+		if(dataCard[i].isHit) {
+			dataCardHit.push(dataCard[i]);
+		}
+	}
+	renderProducts(dataCardHit, '.hot-offers');
+}
+
+var productList = document.querySelector('.catalog-products');
+if (productList) {
+	renderProducts(dataCard, '.catalog-products');
+}
+
+/*модальное окно поиска*/
+
+var search = document.querySelector(".main-search");
+var searchPopup = document.querySelector(".modal-search");
+var searchField = searchPopup.querySelector("input");
+
+
+
+search.addEventListener('click', function(event) {
+	event.preventDefault();
+	searchPopup.classList.toggle("modal-show");
+	search.classList.toggle("active");
+	searchField.focus();
+});
+
+
+
+var login = document.querySelector(".header-login");
+var loginPopup = document.querySelector(".modal-login");
+var loginField = loginPopup.querySelector('[name="login"]');
+var passwordField = loginPopup.querySelector('[name="password"]');
+var userLogin = localStorage.getItem("userLogin");
+var loginForm = loginPopup.querySelector('form');
+
+login.addEventListener('click', function(event) {
+	event.preventDefault();
+	loginPopup.classList.remove("modal-error");
+	loginPopup.classList.toggle("modal-show");
+	login.classList.toggle("active");
+	if(userLogin) {
+		loginField.value = userLogin;
+		passwordField.focus();
+	} else {
+		loginField.focus();
+	}
+});
+
+loginForm.addEventListener("submit", function(event) {
+	
+	if (!loginField.value || !passwordField.value) {
+		event.preventDefault();
+		loginPopup.classList.add("modal-error");
+		
+	} else {
+		localStorage.setItem("userLogin", loginField.value);
+	}
+});
 
 
 
 
+
+
+
+window.addEventListener("keydown", function(event){
+	if(event.keyCode === 27) {
+		if (searchPopup.classList.contains("modal-show")) {
+			searchPopup.classList.remove("modal-show");
+			search.classList.remove("active");
+		} else if (loginPopup.classList.contains("modal-show")) {
+			loginPopup.classList.remove("modal-show");
+			login.classList.remove("active");
+		}
+	}
+});
